4bf9061b1e4f5d4df113fb933ddc8dc730ab6c3b
steincastillo <stein@americamail.com>
2016-12-09 22:27:57 +0100

    tried different algorithms

--- detect_circles.py
+++ detect_circles.py
@@ -6,6 +6,7 @@
 import numpy as np
 import argparse
 import cv2
+import mahotas
 
 print("\n")
 print("**************************************")
@@ -27,28 +28,32 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.medianBlur(gray, 5)
-cblurred = cv2.cvtColor(blurred, cv2.COLOR_GRAY2BGR)
+
+#Threshold the image
+thresh = blurred.copy()
+T = mahotas.thresholding.otsu(thresh)
+thresh[thresh > T] = 255
+thresh[thresh < 255] = 0
+thresh = cv2.bitwise_not(thresh)
+
+
 
 #detect circles
-circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, 120, param1=100,
-			param2=30, minRadius=0, maxRadius=0)
+#circles = cv2.HoughCircles(thresh, cv2.HOUGH_GRADIENT, 1, 120, param1=100,
+#			param2=30, minRadius=0, maxRadius=0)
+
+circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1.6, 100)
 circles = np.uint16(np.around(circles))
 
-for i circles[0,:]:
+for i in circles[0,:]:
 	#Draw the outer circle
 	cv2.circle(image, (i[0], i[1]), i[2], (0,255,0), 2)
 	#draw center of the circle
 	cv2.circle(image, (i[0], i[1]), 2, (0, 255, 0), 3)
 
-cv2.imshow("HoughCircles", image)
-cv2.waitKey()
-cv2.destroyAllWindows()
+cv2.imshow("Otsu", thresh)
+cv2.imshow("Hough Circles", image)
 
-eq = cv2.equalizeHist(image)
-
-# Show our images -- notice how the constrast of the second
-# image has been stretched
-cv2.imshow("Histogram Equalization", np.hstack([image, eq]))
 key = cv2.waitKey(0) &0xFF
 
 #Save the file?
@@ -62,5 +67,5 @@ if key == ord("s"):
     print ("Saving: ", savefile)
     cv2.imwrite(savefile, eq)
 
-
+cv2.destroyAllWindows()
   
