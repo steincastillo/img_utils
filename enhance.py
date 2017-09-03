05a4752e80a9776c5e965bfa8c48f28ca159aa39
steincastillo <stein@americamail.com>
2017-09-03 11:55:34 +0200

    updated code comments

--- enhance.py
+++ enhance.py
@@ -10,22 +10,23 @@ Created on Sat Sep  2 10:20:25 2017
 *****************************************
 
 Adjustment parameters:
-    alpha: Adjust contrast. Default value: 1.5
+    alpha: Adjust sharpness. Default value: 1.5
     beta: not used on this version. Default value: 0
     gamma: adjust brightness. Default value: 0
 
 Usage: 
     python enhance.py -i <imageFile> -> Apply default values
     python enhance.py -i <imageFile> --alpha 2 --beta 50
-    python enhance.py -i <imageFile> -a 2 -b 50
-    python enhance.py -i <imageFile> -a 1 -b 50 : only adjust brigthness
-    python enhance.py -i <imageFile> -a 2 : only adjust contrast
+    python enhance.py -i <imageFile> -a 2 -g 50
+    python enhance.py -i <imageFile> -a 1 -g 50 : only adjust brightness
+    python enhance.py -i <imageFile> -a 2 : only adjust sharpness
 
 Examples:
     python enhance.py -i <imageFile> -a 0.5 -b 120 : X-ray effect
     
 """
 
+# Import libraries
 import cv2 as cv
 import os.path
 import argparse
@@ -36,12 +37,16 @@ print (__doc__)
 # Parse the arguments
 ap = argparse.ArgumentParser(description="Quick image enahancer")
 ap.add_argument("-i", "--image", required=True, help ="Path to the image")
+
 ap.add_argument("-a", "--alpha", required=False, default = 1.5, 
                 help = "alpha value")
+
 ap.add_argument("-b", "--beta", required=False, default = 0,
                 help = "beta value")
+
 ap.add_argument("-g", "--gamma", required = False, default = 0, 
                 help = "gamma value")
+
 args = vars(ap.parse_args())
 
 # Verify the file exists
@@ -49,22 +54,32 @@ if not(os.path.isfile(args["image"])):
     print ("[Error] File {} does not exist. Please verify\n".format(args["image"]))
     exit(0)
 
+# Open the image file
 image = cv.imread(args["image"])
 
+# Unpack the editing parameters
 alpha = float(args["alpha"])
-beta = float(args["beta"])
+beta  = float(args["beta"])
 gamma = float(args["gamma"])
 
+# result = ((image * alpha) + (np.zeros * beta) + gamma)
+# alpha: changes the sharpness of the image
+# beta: Does nothing since it is multipied to an array of zeros
+# gamma: changes the brigthness of the image
 result = cv.addWeighted(image, alpha, np.zeros(image.shape, image.dtype), beta, gamma)
+
+# The next section was used to experiment with 2d convolution
 #kernel = np.array([[-1,-1,-1], 
 #                   [-1,9,-1], 
 #                   [-1,-1,-1]])
     
-kernel = np.array([[0,-1, 0], 
-                   [-1,5,-1], 
-                   [0,-1, 0]])
+#kernel = np.array([[0,-1, 0], 
+#                   [-1,5,-1], 
+#                   [0,-1, 0]])
     
 #result = cv.filter2D(image, -1, kernel)
+
+print ("Press any key to quit")
 
 cv.namedWindow("Original", cv.WINDOW_NORMAL)
 cv.imshow("Original", image)
@@ -72,5 +87,4 @@ cv.namedWindow("Enhanced", cv.WINDOW_NORMAL)
 cv.imshow("Enhanced", result)
 
 key = cv.waitKey(0) &0xFF
-
 cv.destroyAllWindows()
