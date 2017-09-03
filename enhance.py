b30359c038379b699c834fff02c0c0e5cb8ebf65
steincastillo <stein@americamail.com>
2017-09-03 12:54:32 +0200

    added code comments

--- enhance.py
+++ enhance.py
@@ -10,16 +10,16 @@ Created on Sat Sep  2 10:20:25 2017
 *****************************************
 
 Adjustment parameters:
-    alpha: Adjust sharpness. Default value: 1.5
+    alpha: Adjust contrast. Default value: 1.2
     beta: not used on this version. Default value: 0
     gamma: adjust brightness. Default value: 0
 
 Usage: 
     python enhance.py -i <imageFile> -> Apply default values
-    python enhance.py -i <imageFile> --alpha 2 --beta 50
+    python enhance.py -i <imageFile> --alpha 2 --gamma 50
     python enhance.py -i <imageFile> -a 2 -g 50
     python enhance.py -i <imageFile> -a 1 -g 50 : only adjust brightness
-    python enhance.py -i <imageFile> -a 2 : only adjust sharpness
+    python enhance.py -i <imageFile> -a 2 : only adjust contrast
 
 Examples:
     python enhance.py -i <imageFile> -a 0.5 -b 120 : X-ray effect
@@ -38,14 +38,14 @@ print (__doc__)
 ap = argparse.ArgumentParser(description="Quick image enahancer")
 ap.add_argument("-i", "--image", required=True, help ="Path to the image")
 
-ap.add_argument("-a", "--alpha", required=False, default = 1.5, 
-                help = "alpha value")
+ap.add_argument("-a", "--alpha", required=False, default = 1.2, type = float,
+                help = "alpha value - Contrast")
 
-ap.add_argument("-b", "--beta", required=False, default = 0,
-                help = "beta value")
+ap.add_argument("-b", "--beta", required=False, default = 0, type = float,
+                help = "beta value - Not used")
 
-ap.add_argument("-g", "--gamma", required = False, default = 0, 
-                help = "gamma value")
+ap.add_argument("-g", "--gamma", required = False, default = 0, type = float,
+                help = "gamma value - Brightness")
 
 args = vars(ap.parse_args())
 
@@ -63,7 +63,7 @@ beta  = float(args["beta"])
 gamma = float(args["gamma"])
 
 # result = ((image * alpha) + (np.zeros * beta) + gamma)
-# alpha: changes the sharpness of the image
+# alpha: changes the contrast of the image
 # beta: Does nothing since it is multipied to an array of zeros
 # gamma: changes the brigthness of the image
 result = cv.addWeighted(image, alpha, np.zeros(image.shape, image.dtype), beta, gamma)
