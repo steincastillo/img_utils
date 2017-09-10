3047afa6f48b0639fc05add23ee46fde245ba04b
steincastillo <stein@americamail.com>
2017-09-10 12:20:23 +0200

    Added filter utility

---
+++ filter.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 10 10:40:17 2017
+
+@author: Stein
+
+*****************************************
+*         Image Filter Utility          *
+*                 V1.0                  *
+*****************************************
+
+Usage:
+    python effect.py -i <imageFile> -f <filter>
+    python coloreq.py --image <imageFile> --filter <filter>
+    
+filter options:
+    sharpen: Sharpen the image
+    gray: Convert to gray scale
+*****************************************
+"""
+
+# Impoer libraries
+import cv2 as cv
+import numpy as np
+import argparse
+import os.path
+
+# Display the header
+print (__doc__)
+
+# Construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser(description = "Image filter utility")
+
+ap.add_argument("-i",
+                "--image",
+                required = True,
+                help = "Path to the image")
+
+ap.add_argument("-f",
+                "--filter",
+                required = True,
+                help = "Effect to apply to the image")
+
+args = vars(ap.parse_args())
+
+# Verify if the file exists
+if not(os.path.isfile(args["image"])):              
+    print ("[Error] File {} does not exist. Please verify\n".format(args["image"]))
+    exit(0)
+    
+# Open the image
+image = cv.imread(args["image"])
+
+# Verify input image is a color image
+height, width, channels = image.shape
+#if channels >= 3 :
+#    image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+filter = args["filter"].lower()
+action = False
+
+# SHARPEN
+if filter == "sharpen":
+    kernel = np.array([[0, -1, 0],
+                       [-1, 5, -1],
+                       [0, -1, 0]])
+    img_output = cv.filter2D(image, -1, kernel)
+    action = True
+ 
+# convert to grayscale
+elif filter == "gray":
+    if channels >= 3 :
+        img_output = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        action = True
+    else: 
+        print ("[ERROR]: Image is not color. Please verify")
+        
+else:
+    print ("[ERROR] Filter option invalid. Please verify")
+    
+# Present the results?
+if action:
+    cv.namedWindow(args["image"], cv.WINDOW_NORMAL)
+    cv.namedWindow(filter.upper(), cv.WINDOW_NORMAL)
+    cv.imshow(args["image"], image)
+    cv.imshow(filter.upper(), img_output)
+    
+    key = cv.waitKey(0)
+    
+    cv.destroyAllWindows()
+    
+    
