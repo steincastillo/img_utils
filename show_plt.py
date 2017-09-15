bcb0ed1ee4322023f6101f4f86dd4c57a7b30dbb
steincastillo <stein@americamail.com>
2017-09-15 21:29:01 +0200

    Moved procedure to functions

--- show_plt.py
+++ show_plt.py
@@ -7,12 +7,12 @@ Edited on 9/4/2017 1:07:16 PM
 
 **************************************
 *            SHOW IMAGE              *
-*                                    *
-*            Version 1.7             *
+*            MATPLOTLIB              *
+*            Version 1.0             *
 **************************************
 
-Usage:  python show_image.py --image <imageFile>
-        python show_image.py -i <imageFile>
+Usage:  python show_plt.py --image <imageFile>
+        python show_plt.py -i <imageFile>
 ***************************************
 """
 
@@ -22,44 +22,61 @@ import os.path
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-# Print routine header
-print (__doc__)
+# Define functions
+def show_image(img):
+    _,_,c = img.shape
+    if c >= 3:    
+        img = cv.cvtColor(img,cv.COLOR_BGR2RGB)  # Convert the image to RGB format
+    plt.imshow(img)
+    plt.xticks([]), plt.yticks([])
+    plt.show()
 
-# Parse the arguments
-ap = argparse.ArgumentParser(description="Display a picture on the screen")
+# Calculate the mean brightness of the image
+def mean_brigh(img):
+    _,_,c = img.shape
+    if c>= 3:
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # Convert image to grayscale
+    return (cv.mean(img))
 
-ap.add_argument("-i",
-                "--image",
-                required=True,
-                help ="Path to the image")
 
-args = vars(ap.parse_args())
+# Main loop
+if __name__=="__main__":
+    # Print routine header
+    print (__doc__)
 
-# Verify the file exists
-if not(os.path.isfile(args["image"])):              
-    print ("[ERROR] File {} does not exist. Please verify.".format(args["image"]))
-    exit(0)
+    # Parse the arguments
+    ap = argparse.ArgumentParser(description="Display a picture on the screen")
 
-image = cv.imread(args["image"])
-(h, w, c) = image.shape
-size = image.size
-imgtype = image.dtype
-image1 = cv.cvtColor(image, cv.COLOR_BGR2GRAY)  # Convert image to grayscale
-means = cv.mean(image1)
+    ap.add_argument("-i",
+                    "--image",
+                    required=True,
+                    help ="Path to the image")
 
-print ("Image properties:")
-print ("-----------------")
-print ("* File: {}".format(args["image"]))
-print ("* Witdh: {}".format (w))
-print ("* Height: {}".format(h))
-print ("* Channels: {}".format(c))
-print ("* Pixels: {:.2f} M".format(size/1e6))
-print ("* File type: {}".format(imgtype))
-print ("* Mean brightness: {}".format(int(means[0])))
-print ("-----------------")
-print ("\n")
+    args = vars(ap.parse_args())
 
-image = cv.cvtColor(image,cv.COLOR_BGR2RGB)
-plt.imshow(image)
-plt.xticks([]), plt.yticks([])
-plt.show()
+    # Verify the file exists
+    if not(os.path.isfile(args["image"])):              
+        print ("[ERROR] File {} does not exist. Please verify.".format(args["image"]))
+        exit(0)
+
+    image = cv.imread(args["image"])
+    (h, w, c) = image.shape
+    size = image.size
+    imgtype = image.dtype    
+    means = mean_brigh(image)
+
+    # Display image properties
+    print ("Image properties:")
+    print ("-----------------")
+    print ("* File: {}".format(args["image"]))
+    print ("* Witdh: {}".format (w))
+    print ("* Height: {}".format(h))
+    print ("* Channels: {}".format(c))
+    print ("* Pixels: {:.1f} M".format(size/1e6))
+    print ("* File type: {}".format(imgtype))
+    print ("* Mean brightness: {}".format(int(means[0])))
+    print ("-----------------")
+    print ("\n")
+
+    # Display the image
+    show_image(image)
